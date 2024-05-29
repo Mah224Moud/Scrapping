6577c14652a6e6967bd23e89c8ef75968a41c75b
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2024-05-29 08:09:51 +0200

    Init project and tooked the categories

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+
+def save_file(filename: str, content: str):
+    with open(filename, "w") as f:
+        f.write(content)
+
+
+response = requests.get("https://books.toscrape.com/")
+content = response.text
+soup = BeautifulSoup(content, "html.parser")
+save_file("html_content.txt", soup.prettify())
+
+cat_div = soup.find("div", class_="side_categories").find("ul").find("li").find("ul")
+
+categories = [category.text.strip() for category in cat_div.children if category.name]
+pprint(categories)
+
