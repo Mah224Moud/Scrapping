65f38f861c5be9cd0e0d6092e08c1297948bcac2
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2024-06-05 22:40:07 +0200

    Added list sorting books list

--- main.py
+++ main.py
@@ -8,6 +8,28 @@ def save_file(filename: str, content: str):
         f.write(content)
 
 
+def count_books_per_category(caterory: str, index: int, total: dict):
+    url = f"https://books.toscrape.com/catalogue/category/books/{caterory}_{index}/index.html"
+    response = requests.get(url)
+    content = response.text
+    soup = BeautifulSoup(content, "html.parser")
+
+    number = soup.find("form", class_="form-horizontal").find("strong").text
+    number = int(number)
+    total[caterory] = number
+
+    print(f"{caterory.replace('-', ' ').capitalize()} contient: {number} livres")
+
+
+def show_categories_that_has_less_books(categories: dict, limit: int) -> dict:
+    print(f"\nListes des catégories de livres contenant moins de {limit+1} livres")
+    result = {}
+    for category in categories:
+        if categories.get(category) <= limit:
+            result[category] = categories.get(category)
+    return result
+
+
 response = requests.get("https://books.toscrape.com/")
 content = response.text
 soup = BeautifulSoup(content, "html.parser")
@@ -31,3 +53,12 @@ for article in articles:
 
 save_file("images.txt", ",\n".join(images))
 save_file("titles.txt", ",\n".join(titles))
+
+numbers_of_books = {}
+for i, category in enumerate(categories, 2):
+    category = category.replace(" ", "-").lower()
+    count_books_per_category(category, i, numbers_of_books)
+
+
+not_enough = show_categories_that_has_less_books(numbers_of_books, 5)
+pprint(not_enough)
